801346d034fd7db2c45d1250eb6f5527329acd27
LukeN <luke.nizik@gmail.com>
2020-03-09 17:52:36 +0100

    Migrated main template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 
 function App() {
+  const subpages = Object.keys(routes).map(function(route) {
+    const current = routes[route];
+    return (
+        <Route path={current.url}>{current.page}</Route>
+    );
+  });
+
+  function Header() {
+    return (
+        <header class="business-header"></header>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <Router>
+        <MainMenu/>
+        <Switch>
+          {subpages}
+        </Switch>
+      </Router>
+      <Footer/>
     </div>
   );
 }
